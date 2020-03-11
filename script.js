f536d5d5ddc6617b0feb13165cf858022ef5d4e3
Web Academie <wacademie@localhost.localdomain>
2020-03-11 10:40:54 +0100

    ajout des checks win

--- script.js
+++ script.js
@@ -57,7 +57,6 @@ $(document).ready(function() {
 					let identif = $(this).attr('id');
 					identif = parseInt(identif);
 					next = identif+X;
-					console.log(next);
 					while ($('#'+next).is(':empty')){
 						next += X;
 					}
@@ -65,12 +64,12 @@ $(document).ready(function() {
 					
 	
 					if (player == 1) {
-						$('#'+next).append('<p> </>');
+						$('#'+next).append('<p></p>');
 						$('#'+next).css('backgroundColor', 'red');
 						player = 2;
 					}
 					else{
-						$('#'+next).append('<p> </p>').css('textAlign', 'center');
+						$('#'+next).append('<p></p>').css('textAlign', 'center');
 						$('#'+next).css('backgroundColor', 'Yellow');
 						player = 1;
 					}
@@ -78,6 +77,120 @@ $(document).ready(function() {
 				else{
 					alert('Case deja remplie !');
 				}
+				// Variables pour voir le backgroundColor du jeton posé
+				let currentcolor = $('#'+next).css('backgroundColor');
+				// Variables pour voir a pos du click +Y
+				let VerifyY1 = next+X;
+				let VerifyY2 = next+(X*2);
+				let VerifyY3 = next+(X*3);
+
+				// Variables pour voir a pos du click +X
+				let VerifyX1 = next+1;
+				let VerifyX2 = next+2;
+				let VerifyX3 = next+3;
+				// Variables pour voir a pos du click -X
+				let VerifyX1less = next-1;
+				let VerifyX2less = next-2;
+				let VerifyX3less = next-3;
+
+				/*
+				let posright = next+1;
+				let posleft = next-1;
+				let posupper = next-X;
+				let posdown = next+X
+				*/
+				// Variables pour voir a pos du click -X en diagonale
+				let VerifyDiagX1 = next-X+1;
+				let VerifyDiagX2 = next-(X*2)+2;
+				let VerifyDiagX3 = next-(X*3)+3;
+
+				let VerifyDiagX1less = next-X-1;
+				let VerifyDiagX2less = next-(X*2)-2;
+				let VerifyDiagX3less = next-(X*3)-3;
+
+				let VerifyDiagY1 = next+X+1;
+				let VerifyDiagY2 = next+(X*2)+2;
+				let VerifyDiagY3 = next+(X*3)+3;
+			
+				let VerifyDiagY1less = next+X-1;
+				let VerifyDiagY2less = next+(X*2)-2;
+				let VerifyDiagY3less = next+(X*3)-3;
+			
+				if ($('#'+VerifyY1).css('backgroundColor') == currentcolor && $('#'+VerifyY2).css('backgroundColor') == currentcolor && $('#'+VerifyY3).css('backgroundColor') == currentcolor) {
+					
+					if (currentcolor == 'rgb(255, 0, 0)') {
+						alert('Joueur 1 Gagne !');
+						score_J1++;
+					}
+					else{
+						alert('Joueur 2 gagne !');
+						score_J2++;
+					}
+				}
+				if ($('#'+VerifyX1).css('backgroundColor') == currentcolor && $('#'+VerifyX2).css('backgroundColor') == currentcolor && $('#'+VerifyX3).css('backgroundColor') == currentcolor) {
+					if (currentcolor == 'rgb(255, 0, 0)') {
+						alert('Joueur 1 Gagne !');
+						score_J1++;
+					}
+					else{
+						alert('Joueur 2 gagne !');
+						score_J2++;
+					}	
+				}
+				if ($('#'+VerifyX1less).css('backgroundColor') == currentcolor && $('#'+VerifyX2less).css('backgroundColor') == currentcolor && $('#'+VerifyX3less).css('backgroundColor') == currentcolor) {
+					if (currentcolor == 'rgb(255, 0, 0)') {
+						alert('Joueur 1 Gagne !');
+						score_J1++;
+					}
+					else{
+						alert('Joueur 2 gagne !');
+						score_J2++;
+					}	
+				}
+				if ($('#'+VerifyDiagX1).css('backgroundColor') == currentcolor && $('#'+VerifyDiagX2).css('backgroundColor') == currentcolor && $('#'+VerifyDiagX3).css('backgroundColor') == currentcolor) {
+					if (currentcolor == 'rgb(255, 0, 0)') {
+						alert('Joueur 1 Gagne !');
+						score_J1++;
+					}
+					else{
+						alert('Joueur 2 gagne !');
+						score_J2++;
+					}	
+				}
+
+				if ($('#'+VerifyDiagY1).css('backgroundColor') == currentcolor && $('#'+VerifyDiagY2).css('backgroundColor') == currentcolor && $('#'+VerifyDiagY3).css('backgroundColor') == currentcolor) {
+					if (currentcolor == 'rgb(255, 0, 0)') {
+						alert('Joueur 1 Gagne !');
+						score_J1++;
+					}
+					else{
+						alert('Joueur 2 gagne !');
+						score_J2++;
+					}	
+				}
+
+				if ($('#'+VerifyDiagX1less).css('backgroundColor') == currentcolor && $('#'+VerifyDiagX2less).css('backgroundColor') == currentcolor && $('#'+VerifyDiagX3less).css('backgroundColor') == currentcolor) {
+					if (currentcolor == 'rgb(255, 0, 0)') {
+						alert('Joueur 1 Gagne !');
+						score_J1++;
+					}
+					else{
+						alert('Joueur 2 gagne !');
+						score_J2++;
+					}	
+				}
+				if ($('#'+VerifyDiagY1less).css('backgroundColor') == currentcolor && $('#'+VerifyDiagY2less).css('backgroundColor') == currentcolor && $('#'+VerifyDiagY3less).css('backgroundColor') == currentcolor) {
+					if (currentcolor == 'rgb(255, 0, 0)') {
+						alert('Joueur 1 Gagne !');
+						score_J1++;
+					}
+					else{
+						alert('Joueur 2 gagne !');
+						score_J2++;
+					}	
+				}
+
+
 				});
 			}
 		}
